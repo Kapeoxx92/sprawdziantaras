35fed692b45f928d8625b84f5aa07e6d21278e44
Kapeoxx92 <kacperwojtczuk92@gmail.com>
2023-04-13 10:42:27 +0200

    second commit

--- test js.js
+++ test js.js
@@ -1,2 +1,13 @@
-function ll()
-if 
+function kolorzmiana(){
+    const lewygorny = document.querySelector('#numer1');
+    
+    if (lewygorny.style.backgroundColor == 'red') {
+      lewygorny.style.backgroundColor = 'green';
+    } else {
+      lewygorny.style.backgroundColor = 'red';
+    }
+    }
+    
+function zmniejszenie() {
+    onclick== lewydolny
+}
